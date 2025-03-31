2a482cc0f27e323d2edbcef996ccc23f59ec3522
abhinav kumar <abhinavv0179@gmail.com>
2025-03-31 06:41:43 +0530

    update the chnages

--- test_app.py
+++ test_app.py
@@ -1,4 +1,16 @@
+import unittest
 from app import hello_world
 
-def test_hello_world():
-    assert hello_world() == "Hello, World!"
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, World!")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# from app import hello_world
+
+# def test_hello_world():
+#     assert hello_world() == "Hello, World!"
+
